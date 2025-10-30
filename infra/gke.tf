dfea1db8eb5f43021a532e653fcb228d075b5191
jyong0 <comrry2@naver.com>
2025-10-31 01:33:08 +0900

    gke nodepool update

--- infra/gke.tf
+++ infra/gke.tf
@@ -1,4 +1,4 @@
-# GKE Standard 클러스터 (노드풀 분리 생성)
+# === GKE Standard 클러스터 ===
 resource "google_container_cluster" "cluster" {
   name     = var.cluster_name
   location = var.region
@@ -6,20 +6,20 @@ resource "google_container_cluster" "cluster" {
   network    = google_compute_network.vpc.self_link
   subnetwork = google_compute_subnetwork.subnet.self_link
 
-  # VPC-native
+  # ✅ VPC Native Networking (Pod CIDR / Service CIDR 사용)
+  networking_mode = "VPC_NATIVE"
+
   ip_allocation_policy {
     cluster_secondary_range_name  = "pods-range"
     services_secondary_range_name = "services-range"
   }
 
-  networking_mode = "VPC_NATIVE"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
   release_channel {
     channel = "REGULAR"
   }
-
-  # 기본 노드풀 비활성화 (별도 node pool 만들기 위해)
-  remove_default_node_pool = true
-  initial_node_count       = 1
 
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
@@ -28,31 +28,39 @@ resource "google_container_cluster" "cluster" {
   depends_on = [google_project_service.services]
 }
 
+# === 비용 최적화 Node Pool ===
 resource "google_container_node_pool" "nodes" {
-  name       = "${var.cluster_name}-pool"
-  location   = var.region
-  cluster    = google_container_cluster.cluster.name
+  name     = "${var.cluster_name}-pool"
+  location = var.region
+  cluster  = google_container_cluster.cluster.name
 
-  node_count = var.node_count
+  node_count = 1
 
   node_config {
-    machine_type = var.machine_type
+    machine_type = "e2-small"
     disk_size_gb = 20
+    image_type   = "COS_CONTAINERD"
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
-    # 컨테이너 최적화 이미지
-    image_type = "COS_CONTAINERD"
+
     labels = {
       role = "app"
     }
+
     metadata = {
       disable-legacy-endpoints = "true"
     }
   }
 
   autoscaling {
-    min_node_count = 2
-    max_node_count = 4
+    min_node_count = 1
+    max_node_count = 2
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
   }
 }
